541a3c35339654bd9e20b2521519ecdc280118e3
popaionut416-jpg <popaionut416@gmail.com>
2026-02-03 22:11:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Ma faci intradevar cel mai fericit barbat di lume 💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Abia astept sa petrecem ziua indragostitilor impreuna cu tine amore 💓💓💓 ",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
@@ -69,8 +69,8 @@ const CONFIG = {
     animations: {
         floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
-        bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
+        bounceSpeed: "0.3s",            // Speed of bouncing animations (0.3-0.7s recommended)
+        heartExplosionSize: 1.2         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
